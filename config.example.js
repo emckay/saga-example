be3afd9a41bc92282d1fbcc9c0a52a64b73483d2
Eric <ericmckay4@gmail.com>
2016-07-11 15:39:45 -0700

    clean up firebase config

--- config.example.js
+++ config.example.js
@@ -1,36 +1,10 @@
 /*
-Get this information from the firebase console
-Make sure that you enable the read permission on your database
-You'll also need to insert some phrases into the database.
-
-This is what the database of the sample application looks like:
-
-{
-   "speechKeys":[
-      "gettysburg",
-      "moon"
-   ],
-   "speeches":{
-      "gettysburg":[
-         "Four score and seven years ago",
-         "our fathers brought forth on this continent,",
-         "a new nation,",
-         "conceived in Liberty,",
-         "and dedicated to the proposition that all men are created equal."
-      ],
-      "moon":[
-         "We choose to go to the moon.",
-         "We choose to go to the moon in this decade  and do the other things,",
-         "not because they are easy,",
-         "but because they are hard,",
-         "because that goal will serve to organize and measure the best of our energies and skills,",
-         "because that challenge is one that we are willing to accept,",
-         "one we are unwilling to postpone,",
-         "and one which we intend to win,",
-         "and the others, too."
-      ]
-   }
-}
+* Get this information from the Firebase console.
+* Make sure that you enable the read permission on your database.
+* You'll also need to insert some phrases into the database.
+*
+* See the database.example.json file for an example.
+* This file can be imported directly to a Firebase database.
 */
 
 export const firebaseConfig = {
